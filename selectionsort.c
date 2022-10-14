a12996f8f29fc28621bd2c9e2aca9acb5c96191e
leiki <leiki@ciandt.com>
2022-10-14 20:43:05 -0300

    refatoracao selectionsort

--- selectionsort.c
+++ selectionsort.c
@@ -1,136 +1,119 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include "file_aux.h"
 
 typedef struct registro registro;
-int contador_comparacoes = 0;
-int contador_troca = 0;
+long long int contador_comparacoes = 0;
+long long int contador_troca = 0;
 
 struct registro {
 	int chave;
-	//além de outros campos de informação do registro
 };
 
-void selectionsort (registro* v, int n)
-{
+void selectionsort (registro* v, int n) {
   int i, j, menor;
   registro temp;
-  for (i = 0; i < n - 1; i++)
-  {
-    menor = i; //troca
+	
+  for (i = 0; i < n - 1; i++) {
+    menor = i;
     contador_troca++;
-
-    for (j = i + 1 ; j < n; j++)
-      // comparação a seguir
+		
+    for (j = i + 1 ; j < n; j++) { 
       contador_comparacoes++;
-      if ( v[j].chave < v[menor].chave)
+      if ( v[j].chave < v[menor].chave) {
         menor = j;
-
-    temp = v[menor]; // troca
+			}
+		}
+		
+    temp = v[menor];
     contador_troca++;
     v[menor] = v[i];
     v[i] = temp;
   }
 }
 
-int main()
-{
-	//registro v[500000];
-	registro *v;
+registro *generate_middle_time_array(int array_size) {
+	registro *array = (registro *)malloc(array_size * sizeof(registro));
+	
+	srand(time(NULL));
+	
+	for (int i = 0; i< array_size; i += 2){
+		array[i].chave = i;
+		array[i + 1].chave = array_size - i;
+	}
+	
+	return array;
+}
+
+registro *generate_worst_time_array(int array_size) {
+	registro *array = (registro *)malloc(array_size * sizeof(registro));
+	
+	for(int i = array_size, j = 0;  i > 0, j < array_size; i --, j++){
+		array[j].chave = i;
+	}
+	
+	return array;
+}
+
+int main(int argc, char **argv) {
+	if(argv[1] == NULL) {
+		return 0;
+	}
+	
 	int i = 0, j = 0;
-	const int n = 100000;
-
-	//srand(time(NULL));
-	srand(1);
-
-	v = malloc (sizeof(registro)*n);
-	if (v==NULL){
-		printf("Nao foi possivel alocar");
-	}
-
-
-//--------------------------------------------------------------------
-//		Random vector
-	/*
-	for (i = 0; i < n; i++) {
-		v[i].chave = rand() % 100;
-		//printf("%d ", v[i].chave);
-	}*/
-	for ( i =0; i< n ;i+=2){
-		v[i].chave = i;
-		v[i+1].chave = n-i;
-
-	}
-
-
-
-//--------------------------------------------------------------------
-
-// caso medio
-
-	printf("Caso medio \n");
-
+	analysis_parameters parameters = init_analysis_parameters();
+	const int array_size = atoi(argv[1]);
+	registro *v = generate_middle_time_array(array_size);
+	
+	create_file();
+	
+	// CASO MEDIO
 	clock_t begin = clock();
-	selectionsort(v, n);
+	selectionsort(v, array_size);
 	clock_t end = clock();
-
-	printf("\n %lf ", begin);
-	printf("\n %lf ", end);
-
-	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-	printf("\n %lf \n", time_spent);
-
-	printf("\n Trocas: %d\n Comparacoes: %d", contador_troca, contador_comparacoes);
-    contador_troca = 0;
-    contador_comparacoes = 0;
-
-	/*
-	printf("\n\nVetor ordenado\n");
-	for (i = 0; i < 2000; i++)
-		printf("%d ", v[i].chave);
-	*/
-
-// melhor caso
-	printf("\nMelhor Caso");
-
+	
+	float time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
+	parameters.middle_case_time = time_spent;
+	parameters.middle_case_swap_quantity = contador_troca;
+	parameters.middle_case_comparison_quantity = contador_comparacoes;
+	contador_troca = 0;
+	contador_comparacoes = 0;
+	
+	// MELHOR CASO
 	begin = clock();
-	selectionsort(v, n);
+	selectionsort(v, array_size);
 	end = clock();
-
-	printf("\n %lf ", begin);
-	printf("\n %lf ", end);
-
-	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-	printf("\n %lf \n", time_spent);
-
-    printf("\n Trocas: %d\n Comparacoes: %d", contador_troca, contador_comparacoes);
-    contador_troca = 0;
-    contador_comparacoes = 0;
-
-// pior caso
-
-	for( i = n, j = 0 ;  i > 0, j < n; i --, j ++){
-		v[j].chave = i;
-	}
-
-	printf("\nPior Caso");
-
+	
+	time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
+	
+	parameters.best_case_time = time_spent;
+	parameters.best_case_swap_quantity = contador_troca;
+	parameters.best_case_comparison_quantity = contador_comparacoes;
+	contador_troca = 0;
+	contador_comparacoes = 0;
+	
+	free(v);
+	
+	// PIOR CASO
+	v = generate_worst_time_array(array_size);
+	
 	begin = clock();
-	selectionsort(v, n);
+	selectionsort(v, array_size);
 	end = clock();
-
-	printf("\n %lf ", begin);
-	printf("\n %lf ", end);
-
-	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-	printf("\n %lf \n", time_spent);
-
-	printf("\n Trocas: %d\n Comparacoes: %d", contador_troca, contador_comparacoes);
-    contador_troca = 0;
-    contador_comparacoes = 0;
-
-
-//--------------------------------------------------------------------
+	
+	time_spent = (float)(end - begin) / CLOCKS_PER_SEC;
+	
+	parameters.worst_case_time = time_spent;
+	parameters.worst_case_swap_quantity = contador_troca;
+	parameters.worst_case_comparison_quantity = contador_comparacoes;
+	contador_troca = 0;
+	contador_comparacoes = 0;
+	
+	write_parameters_in_file(parameters);
+	
+	close_file();
+	
 	free(v);
 	return 0;
 }
